2bd17b04bd138ad62327b1a17922fb714e5369bd
Boris <D:\The Bat!>
2023-04-15 02:54:50 +0300

    4 задача

---
+++ ex4/Program.cs
@@ -0,0 +1,13 @@
+﻿// chetnie
+Console.Write("Enter number: ");
+int N = int.Parse(Console.ReadLine());
+int x = 2;
+if (N >= x)
+{
+     while (x <= N)
+     {
+        Console.Write($"{x}  ");
+        x = x + 2;
+     }
+}
+else Console.Write("четных чисел нет");
