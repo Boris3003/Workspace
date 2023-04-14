38e8c388a971148694360776cc9204f665187d3f
Boris <D:\The Bat!>
2023-04-15 02:54:13 +0300

    2 задача

---
+++ ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿// maximal figure
+Console.Write("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Enter third number: ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if(b> a) {max = b; if(c> b) {max = c;}}
+else {if(c> a) max = c;}
+Console.WriteLine($"Maximum number: {max}");
