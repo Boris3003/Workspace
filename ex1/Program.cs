8058b480e4872dc6755fde650c12039942f5cc1e
Boris <D:\The Bat!>
2023-04-15 02:53:51 +0300

    1 задача

---
+++ ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿// more figure
+Console.Write("Enter first number: ");
+int first = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int second = int.Parse(Console.ReadLine());
+int min = first;
+int max = second;
+if(first> second)
+{
+    min = second;
+    max = first;
+}
+Console.WriteLine($"Minimum number: {min}");
+Console.WriteLine($"Maximum number: {max}");
